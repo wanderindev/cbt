58c0f807bc22b1fadd6264546c82e0b32e243e7a
Javier Feliu <javier@wanderin.dev>
2021-09-08 21:50:09 -0500

    feat: Add user logout

--- app/routes.py
+++ app/routes.py
@@ -36,3 +36,9 @@ def login():
             next_page = url_for("index")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
