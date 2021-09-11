22bc59a2b0ea12e7a4e3cd24aab5c5358aee058f
Javier Feliu <javier@wanderin.dev>
2021-09-10 22:09:10 -0500

    feat: Add functionality for sending errors by email

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -10,5 +12,23 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = "login"
 migrate = Migrate(app, db)
+
+if not app.debug and app.config["MAIL_SERVER"]:
+    auth = None
+    if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+    secure = None
+    if app.config["MAIL_USE_TLS"]:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+        fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+        toaddrs=app.config["ADMINS"],
+        subject="Microblog Failure",
+        credentials=auth,
+        secure=secure,
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 from app import routes, models, errors
