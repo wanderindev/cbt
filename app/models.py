c446b275214a63b49dea5bf04978b2bba80b96b4
Javier Feliu <javier@wanderin.dev>
2021-09-13 10:35:24 -0500

    feat: Add method for followed posts to user model

--- app/models.py
+++ app/models.py
@@ -58,6 +58,13 @@ class User(UserMixin, db.Model):
             > 0
         )
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 @login.user_loader
 def load_user(id):
